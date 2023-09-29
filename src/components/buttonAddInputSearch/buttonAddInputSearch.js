f489733ae86084324ed6857bc5c350bb0e32598c
Fida-VS <poletaeva69vera@mail.ru>
2023-09-29 09:46:18 +0300

    remove needless file

--- src/components/buttonAddInputSearch/buttonAddInputSearch.js
+++
@@ -1,7 +0,0 @@
-import styles from './buttonAddInputSearch.module.css'
-
-export const ButtonAddInputSearch = () => {
-	return (
-		<button className={styles.buttonAddInputSearch}>🔍</button>
-	);
-};
